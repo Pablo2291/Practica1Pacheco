ccae65879121ca1d20eec3fb198d8d693504f61a
UIMQROO <uimqroo@iMac-de-UIMQROO.local>
2023-03-09 10:16:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practica1pacheco/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.practica1pacheco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edNombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void clic (View vista){
+        edNombre = findViewById(R.id.et_name);
+        String nombre = String.valueOf(edNombre.getText());
+        Toast.makeText(getBaseContext(), "Hola " +nombre, Toast.LENGTH_SHORT).show();
+    }
+}
